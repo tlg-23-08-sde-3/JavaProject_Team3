7924d4173b13227d284bfbd0b30bccbf6b23dcc7
Chandan Chauhan <bellicose100xp@users.noreply.github.com>
2023-09-16 16:46:43 -0700

    Added Board && Updated Spinner Enum

--- src/com/spincity/roulette/Game.java
+++ src/com/spincity/roulette/Game.java
@@ -5,8 +5,13 @@ import com.spincity.roulette.bet.BetCalculator;
 import com.spincity.roulette.bet.BettingFactory;
 
 public class Game {
-    private BettingCategory bettingCategory;
-    private BettingCategory.Color colorOption;
+    private Bet bettingCategory;
+    private Bet.Color colorOption;
+    private Board board;
+
+    public Game() {
+        board = new Board();
+    }
 
     public void gameStats() {
         System.out.println("Account Balance: $2,500.0                                Player: Jojo");
@@ -14,19 +19,20 @@ public class Game {
 
     public void selectBet() {
         gameStats();
-        Board.displayBoard();
+        board.display();
 
         System.out.println("\nEnter bet amount:");
         System.out.println("\nSelect bet category: ");
         System.out.println("\nSelect bet option: ");
 
         // Selection Options
-        bettingCategory = BettingCategory.COLOR;
-        colorOption = BettingCategory.Color.BLACK;
+        bettingCategory = Bet.COLOR;
+        colorOption = Bet.Color.BLACK;
 
         Console.clear();
         gameStats();
-        Board.displayBoard();
+
+        board.display();
 
         Spinner spinner = new Spinner();
         SpinnerNumber pickedNumber = spinner.spin();
