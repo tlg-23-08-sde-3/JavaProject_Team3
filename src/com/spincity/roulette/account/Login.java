2fe5c380592fda3a2c858cb92be1ae4732211746
Chandan Chauhan <bellicose100xp@users.noreply.github.com>
2023-09-19 15:28:20 -0700

    Added banner. Fixed returning user prompt.

--- src/com/spincity/roulette/account/Login.java
+++ src/com/spincity/roulette/account/Login.java
@@ -3,6 +3,8 @@ package com.spincity.roulette.account;
 import com.apps.util.Console;
 import com.apps.util.Prompter;
 import com.spincity.roulette.utils.ANSI;
+import static com.spincity.roulette.utils.ANSI.*;
+import com.spincity.roulette.utils.Banner;
 
 import java.util.Scanner;
 
@@ -16,30 +18,25 @@ public class Login {
     public Player start() {
         Prompter prompter = new Prompter(new Scanner(System.in));
         Console.clear();
-        System.out.println();
-        System.out.println("-------------------------------------");
-        System.out.println("W E L C O M E   T O   T H E   G A M E");
-        System.out.println("-------------------------------------");
-        System.out.println();
+        Banner.banner();
 
         int choice;
         do {
             System.out.println();
-            System.out.println("===================================");
             System.out.println("PLEASE SELECT AN OPTION TO CONTINUE");
-            System.out.println("===================================");
             System.out.println();
             System.out.println("1. Login to existing account");
             System.out.println("2. Create new account");
             System.out.println("3. Exit the Game");
             System.out.println();
-            choice = Integer.parseInt(prompter.prompt("\nMake a selection: " , "(1|2|3)", ANSI.colorRed("\nInvalid choice. Please select 1,2 or 3.")));
+            choice = Integer.parseInt(prompter.prompt("\nMake a selection (1-3): " , "[123]", ANSI.colorRed("\nInvalid choice. Please select 1,2 or 3.")));
 
             switch (choice) {
                 case 1:
                     Console.clear();
+                    Banner.banner();
                     System.out.println();
-                    int accountId = Integer.parseInt(prompter.prompt("Enter your account ID: ", "\\d{4}" , "\nPlease enter your 4 digit account ID\n" ));
+                    int accountId = Integer.parseInt(prompter.prompt("Enter your account ID: ", "\\d{4}" , colorRed("\nPlease enter your 4 digit account ID\n") ));
                     account = Account.getAccount(accountId);
                     if (account == null) {
                         System.out.println();
@@ -47,30 +44,31 @@ public class Login {
                         System.out.println();
                     } else {
                         Console.clear();
+                        Banner.banner();
                         account.getPlayer().setWantsToPlay(true);
                         System.out.println();
                         System.out.println("Welcome back, " + account.getPlayer().getName() + "!");
                         System.out.println("Your account balance is: $" + account.getPlayer().getAccountBalance() );
+                        prompter.prompt("\nPress enter to continue...");
                         System.out.println();
                     }
                     break;
                 case 2:
                     Console.clear();
+                    Banner.banner();
                     System.out.println();
-                    System.out.println("==================");
                     System.out.println("CREATE NEW ACCOUNT");
-                    System.out.println("==================");
                     System.out.println();
                     String playerName = prompter.prompt("Enter your player name: ", "[A-Za-z]+", "\nProvide a valid name\n");
                     int age = Integer.parseInt(prompter.prompt("Enter your age: ", "\\d{1,2}" , "\nAge is not valid to enter the game\n" ));
                     if (age < 21) {
                         System.out.println();
-                        System.out.println("You must be at least 21 years old to play.");
+                        System.out.println(colorRed("You must be at least 21 years old to play."));
                         System.out.println();
                     } else {
                         account = Account.createNewAccount(playerName);
                         System.out.println();
-                        System.out.println("Account created! Your account ID is: " + account.getAccountId() + "\n");
+                        System.out.println(colorGreen("Account created! Your account ID is: " + account.getAccountId() + "\n"));
                         prompter.prompt("Press enter to continue...");
                     }
                     break;
@@ -84,7 +82,7 @@ public class Login {
                     break;
                 default:
                     System.out.println( );
-                    System.out.println("Invalid choice. Please select 1,2 or 3.\n");
+                    System.out.println(colorRed("Invalid choice. Please select 1,2 or 3.\n"));
                     break;
             }
         } while (account == null);
