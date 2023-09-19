76e8ab28a92dd10131d83686d3393492384c4cab
Jojo George Mattam <jojogeorge.mattam@tlgcohort.com>
2023-09-19 16:36:19 -0500

    Added Junit test folders Junit test for even odd SplashScreen new images

---
+++ test/com/spincity/roulette/bet/ColorBetTest.java
@@ -0,0 +1,12 @@
+package com.spincity.roulette.bet;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class ColorBetTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+}
