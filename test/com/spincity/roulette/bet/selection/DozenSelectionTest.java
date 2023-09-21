3be586561d5d503c0a9f490575baea9a0e78f8d7
Jojo George Mattam <jojogeorge.mattam@tlgcohort.com>
2023-09-21 14:09:03 -0500

    Junit Testing for BetSelection Completed

--- test/com/spincity/roulette/bet/selection/DozenSelectionTest.java
+++ test/com/spincity/roulette/bet/selection/DozenSelectionTest.java
@@ -1,7 +1,59 @@
 package com.spincity.roulette.bet.selection;
 
+import com.apps.util.Prompter;
+import com.spincity.roulette.bet.BetType;
+import org.junit.Test;
+
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 import static org.junit.Assert.*;
 
 public class DozenSelectionTest {
+
+    @Test
+    public void whenUserSelected_1_shouldReturn_dozen_1_to_12() {
+        Prompter prompter = new Prompter(new Scanner("1"));
+        DozenSelection dozenSelection = new DozenSelection(prompter);
+        dozenSelection.select();
+
+        assertEquals(BetType.Dozen.DOZEN_1_TO_12, dozenSelection.getBetOption());
+    }
+
+    @Test
+    public void whenUserSelected_2_shouldReturn_dozen_13_to_24() {
+        Prompter prompter = new Prompter(new Scanner("2"));
+        DozenSelection dozenSelection = new DozenSelection(prompter);
+        dozenSelection.select();
+
+        assertEquals(BetType.Dozen.DOZEN_13_TO_24, dozenSelection.getBetOption());
+    }
+
+    @Test
+    public void whenUserSelected_3_shouldReturn_dozen_25_to_36() {
+        Prompter prompter = new Prompter(new Scanner("3"));
+        DozenSelection dozenSelection = new DozenSelection(prompter);
+        dozenSelection.select();
+
+        assertEquals(BetType.Dozen.DOZEN_25_TO_36, dozenSelection.getBetOption());
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenUserSelected_4_shouldReturn_invalidInput() {
+        Prompter prompter = new Prompter(new Scanner("4"));
+        DozenSelection dozenSelection = new DozenSelection(prompter);
+        dozenSelection.select();
+
+        assertNull(dozenSelection.getBetOption());
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenUserSelected_abc_shouldReturn_invalidInput() {
+        Prompter prompter = new Prompter(new Scanner("abc"));
+        DozenSelection dozenSelection = new DozenSelection(prompter);
+        dozenSelection.select();
+
+        assertNull(dozenSelection.getBetOption());
+    }
 
 }
--- test/com/spincity/roulette/bet/selection/SingleNumberSelectionTest.java
+++ test/com/spincity/roulette/bet/selection/SingleNumberSelectionTest.java
@@ -1,7 +1,46 @@
 package com.spincity.roulette.bet.selection;
 
+import com.apps.util.Prompter;
+import com.spincity.roulette.bet.BetType;
+import com.spincity.roulette.spinner.SpinnerNumber;
+import org.junit.Test;
+
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 import static org.junit.Assert.*;
 
 public class SingleNumberSelectionTest {
+
+    @Test
+    public void whenUserSelected_int_1_TO_36_shouldReturn_corresponding_value() {
+        int startNumber = 0;
+        for (BetType.SingleNumber singleNumber : BetType.SingleNumber.values()) {
+            Prompter prompter = new Prompter(new Scanner(String.valueOf(startNumber)));
+            SingleNumberSelection singleNumberSelection = new SingleNumberSelection(prompter);
+            singleNumberSelection.select();
+            System.out.println(singleNumber);
+            startNumber++;
+            assertEquals(singleNumber, singleNumberSelection.getBetOption());
+        }
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenUserSelected_37_shouldReturn_invalidInput() {
+        Prompter prompter = new Prompter(new Scanner("37"));
+        SingleNumberSelection singleNumberSelection = new SingleNumberSelection(prompter);
+        singleNumberSelection.select();
+
+        assertNull(singleNumberSelection.getBetOption());
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenUserSelected_abc_shouldReturn_invalidInput() {
+        Prompter prompter = new Prompter(new Scanner("abc"));
+        SingleNumberSelection singleNumberSelection = new SingleNumberSelection(prompter);
+        singleNumberSelection.select();
+
+        assertNull(singleNumberSelection.getBetOption());
+    }
 
 }
